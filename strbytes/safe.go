dad02a09a5e04419515e0be085faebcdb6bfc468
Drew Boyuka <drew@pendo.io>
2016-03-28 14:03:03 -0400

    fixup! Cond. compilation of unsafe usage + safe fallback

--- strbytes/safe.go
+++ strbytes/safe.go
@@ -2,12 +2,16 @@
 
 package strbytes
 
+import (
+	"strconv"
+)
+
 func Equal(astr string, bbytes []byte) bool {
 	return astr == string(bbytes)
 }
 
 func ParseFloat(bytes []byte, prec int) (float64, error) {
-	return strconv.ParseFloat(string(v), prec)
+	return strconv.ParseFloat(string(bytes), prec)
 }
 
 func ParseInt(bytes []byte, base int, bitSize int) (int64, error) {
