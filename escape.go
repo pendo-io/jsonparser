5f22047c1da176d3a737ac3b43f5eb0d4847a84b
Drew Boyuka <drew@pendo.io>
2023-02-27 17:29:34 -0500

    Clean up upstream patch to fix higher-than-surrogate-pair escapes

--- escape.go
+++ escape.go
@@ -10,9 +10,7 @@ import (
 const supplementalPlanesOffset = 0x10000
 const highSurrogateOffset = 0xD800
 const lowSurrogateOffset = 0xDC00
-
-const basicMultilingualPlaneReservedOffset = 0xDFFF
-const basicMultilingualPlaneOffset = 0xFFFF
+const endSurrogateOffset = 0xE000
 
 func combineUTF16Surrogates(high, low rune) rune {
 	return supplementalPlanesOffset + (high-highSurrogateOffset)<<10 + (low - lowSurrogateOffset)
@@ -52,22 +50,15 @@ func decodeSingleUnicodeEscape(in []byte) (rune, bool) {
 	return rune(h1<<12 + h2<<8 + h3<<4 + h4), true
 }
 
-// isUTF16EncodedRune checks if a rune is in the range for non-BMP characters,
-// which is used to describe UTF16 chars.
-// Source: https://en.wikipedia.org/wiki/Plane_(Unicode)#Basic_Multilingual_Plane
-func isUTF16EncodedRune(r rune) bool {
-	return highSurrogateOffset <= r && r <= basicMultilingualPlaneReservedOffset
-}
-
 func decodeUnicodeEscape(in []byte) (rune, int) {
 	if r, ok := decodeSingleUnicodeEscape(in); !ok {
 		// Invalid Unicode escape
 		return utf8.RuneError, -1
-	} else if r <= basicMultilingualPlaneOffset && !isUTF16EncodedRune(r) {
-		// Valid Unicode escape in Basic Multilingual Plane
+	} else if r < highSurrogateOffset || r >= endSurrogateOffset {
+		// Valid Unicode escape in Basic Multilingual Plane (not part of a surrogate pair)
 		return r, 6
 	} else if r2, ok := decodeSingleUnicodeEscape(in[6:]); !ok { // Note: previous decodeSingleUnicodeEscape success guarantees at least 6 bytes remain
-		// UTF16 "high surrogate" without manditory valid following Unicode escape for the "low surrogate"
+		// UTF16 "high surrogate" without mandatory valid following Unicode escape for the "low surrogate"
 		return utf8.RuneError, -1
 	} else if r2 < lowSurrogateOffset {
 		// Invalid UTF16 "low surrogate"
@@ -120,13 +111,11 @@ func unescapeToUTF8(in, out []byte) (inLen int, outLen int) {
 	return -1, -1
 }
 
-// unescape unescapes the string contained in 'in' and returns it as a slice.
-// If 'in' contains no escaped characters:
-//   Returns 'in'.
-// Else, if 'out' is of sufficient capacity (guaranteed if cap(out) >= len(in)):
-//   'out' is used to build the unescaped string and is returned with no extra allocation
-// Else:
-//   A new slice is allocated and returned.
+// Unescape unescapes the string contained in 'in' and returns it as a slice.
+// If 'in' contains no escaped characters: returns 'in'.
+// Else, if 'out' is of sufficient capacity (guaranteed if cap(out) >= len(in)): 'out' is used to build the unescaped
+// string and is returned with no extra allocation.
+// Else: A new slice is allocated and returned.
 func Unescape(in, out []byte) ([]byte, error) {
 	firstBackslash := bytes.IndexByte(in, '\\')
 	if firstBackslash == -1 {
